e39b6ca764db57faf1d9a1d54b0fc096a9090390
AjT67 <14828@holbertonstudents.com>
2026-07-10 12:23:18 +0000

    Added putchar single digit of base 10 starting at 0 program

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - uses putchar to print the alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
