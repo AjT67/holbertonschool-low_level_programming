33c475a58a2c3ee99704fefcbdd01732391a983c
AjT67 <14828@holbertonstudents.com>
2026-07-10 12:59:02 +0000

    Added putchar base16 in lowercase program

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - putchar base 16 lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i;
+	char c;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
