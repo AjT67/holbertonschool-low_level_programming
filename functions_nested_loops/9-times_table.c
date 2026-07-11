4f42f4576ec0a882b682bbf2f271875f8fc5a9d1
AjT67 <14828@holbertonstudents.com>
2026-07-11 05:54:19 +0000

    hotfix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,32 +4,26 @@
  */
 void times_table(void)
 {
-        int i, j, result;
+	int i, j, result;
 
-        for (i = 0; i <= 9; i++)
-        {
-                for (j = 0; j <= 9; j++)
-                {
-                        result = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
 
-                        if (j > 0)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
+			if (j > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 
-                        if (result >= 10)
-                        {
-                                _putchar((result / 10) + '0');
-                                _putchar((result % 10) + '0');
-                        }
-                        else
-                        {
-                                _putchar(' ');
-                                _putchar(result + '0');
-                        }
-                }
+			if (result >= 10)
+				_putchar((result / 10) + '0');
 
-                _putchar('\n');
-        }
+			_putchar((result % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
 }
