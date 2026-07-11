13ccdf90752d09b52fb30b77067731f390a2e82e
AjT67 <14828@holbertonstudents.com>
2026-07-11 06:03:35 +0000

    added print all integers from n to 98 function

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all integers from n to 98, followed by a newline
+ * @n: the starting number
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+
+		printf("%d\n", n);
+	}
+}
