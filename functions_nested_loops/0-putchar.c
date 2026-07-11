fe87b59ff28a3d5671efb7624eb2bed5439103cd
AjT67 <14828@holbertonstudents.com>
2026-07-11 03:37:59 +0000

    Added putchar each letter in _putchar program

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - uses putchar to print each char in "_putchar"
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
