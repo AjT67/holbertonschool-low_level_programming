9a3c76ed68f8567a9db9c592b7cd0a3474e4fbb0
AjT67 <14828@holbertonstudents.com>
2026-07-15 03:49:26 +0000

    Added a print every other character of a string function

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints half the string
+ * @str: putchar string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = (i + 2);
+	}
+	_putchar('\n');
+}
