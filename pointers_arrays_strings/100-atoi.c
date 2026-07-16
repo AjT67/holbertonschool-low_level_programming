81963ca48b5969032f0c010ff989c344faa64c0f
AjT67 <14828@holbertonstudents.com>
2026-07-16 07:33:59 +0000

    logic hotfix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,16 +12,23 @@ int _atoi(char *s)
 	unsigned int i;
 	int sign;
 	int num;
+	int started;
 
 	i = 0;
 	sign = 1;
 	num = 0;
+	started = 0;
 	while (s[i] != 0)
 	{
 		if (s[i] == '-')
 			sign = (sign * -1);
 		if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
 			num = num * 10 + (s[i] - '0');
+		}
+		if (started == 1 && (s[i] < '0' || s[i] > '9'))
+			break;
 		++i;
 	}
 	return (num * sign);
