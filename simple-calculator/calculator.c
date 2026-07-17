d6a66e7fd942769e76d9104dae9d49f3d9d34f6e
AjT67 <14828@holbertonstudents.com>
2026-07-17 11:17:37 +0000

    added simple calculator menu that exits on 0 with while loop for other int or char

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Calculator menu exiting on 0 with flushing leftover input
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+	int choice;
+
+	printf("Simple Calculator\n");
+	printf("1) Add\n");
+	printf("2) Subtract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Quit\n");
+	printf("Choice:\n");
+	while (scanf("%d", &choice) != 1 || choice != 0)
+	{
+		while ((c = getchar()) != '\n' && c != EOF)
+			;
+		printf("Try again!\n");
+		printf("Choice: ");
+	}
+
+	printf("Bye!\n");
+	return (0);
+}
