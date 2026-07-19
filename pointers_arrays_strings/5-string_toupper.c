43f2cf0b738522af5f70b4e1e691d4e2d83b48e8
AjT67 <14828@holbertonstudents.com>
2026-07-19 05:22:57 +0000

    added convert string to uppercase function

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: the string to convert
+ *
+ * Return: a pointer to the resulting string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+
+	return (str);
+}
