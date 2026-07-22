34245b958a24489f82d6dff2ef4b3216e140de4f
Aj <14828@holbertonstudents.com>
2026-07-22 04:22:16 +0000

    added all prototypes to dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct dog - a struct describing a dog
@@ -8,6 +9,11 @@
  * @age: the age of the dog
  * @owner: the owner of the dog
  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 struct dog
 {
