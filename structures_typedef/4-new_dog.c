2874a84dd198f9704a2e7180c2d9790d917b87bd
Aj <14828@holbertonstudents.com>
2026-07-24 23:59:46 +0000

    hotfix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,28 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * dup_str - creates a heap copy of a string
+ * @str: string to copy
+ *
+ * Return: pointer to the copy, or NULL on failure
+ */
+char *dup_str(char *str)
+{
+	unsigned int i;
+	char *copy;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	copy = malloc(i + 1);
+	if (copy == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		copy[i] = str[i];
+	copy[i] = '\0';
+	return (copy);
+}
 
 /**
  * new_dog - creates a new dog_t
@@ -10,44 +32,30 @@
  *
  * Return: pointer to the new struct, or NULL on failure
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 	char *name_copy;
 	char *owner_copy;
-	unsigned int i;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-
-	name_copy = malloc(strlen(name) + 1);
+	name_copy = dup_str(name);
 	if (name_copy == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-
-	owner_copy = malloc(strlen(owner) + 1);
+	owner_copy = dup_str(owner);
 	if (owner_copy == NULL)
 	{
 		free(d);
 		free(name_copy);
 		return (NULL);
 	}
-
-	for (i = 0; name[i] != '\0'; i++)
-		name_copy[i] = name[i];
-	name_copy[i] = '\0';
-
-	for (i = 0; owner[i] != '\0'; i++)
-		owner_copy[i] = owner[i];
-	owner_copy[i] = '\0';
-
 	d->name = name_copy;
 	d->age = age;
 	d->owner = owner_copy;
-
 	return (d);
 }
