fc96a76504e189a8efd02c6f7693d44ed31c83db
AjT67 <14828@holbertonstudents.com>
2026-08-28 05:58:00 +0000

    hotfix

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,95 +1,64 @@
 #include "variadic_functions.h"
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 
 /**
  * struct printer - matches a format char to its printer function
  * @type: the format character ('c', 'i', 'f', 's')
- * @f: pointer to the function that converts the arg to a string
+ * @f: pointer to the function that prints the arg
  */
 typedef struct printer
 {
 	char type;
-	char *(*f)(va_list);
+	void (*f)(va_list);
 } printer_t;
 
 /**
- * print_char - converts a char argument to a malloc'd string
+ * print_char - prints a char argument
  * @args: the va_list to pull from
  *
- * Return: pointer to the malloc'd string
+ * Return: void
  */
-char *print_char(va_list args)
+void print_char(va_list args)
 {
-	char *str = malloc(sizeof(char) * 2);
-
-	if (str == NULL)
-		return (NULL);
-
-	str[0] = (char)va_arg(args, int);
-	str[1] = '\0';
-
-	return (str);
+	printf("%c", va_arg(args, int));
 }
 
 /**
- * print_int - converts an int argument to a malloc'd string
+ * print_int - prints an int argument
  * @args: the va_list to pull from
  *
- * Return: pointer to the malloc'd string
+ * Return: void
  */
-char *print_int(va_list args)
+void print_int(va_list args)
 {
-	char *str = malloc(sizeof(char) * 12);
-
-	if (str == NULL)
-		return (NULL);
-
-	sprintf(str, "%d", va_arg(args, int));
-
-	return (str);
+	printf("%d", va_arg(args, int));
 }
 
 /**
- * print_float - converts a float argument to a malloc'd string
+ * print_float - prints a float argument
  * @args: the va_list to pull from
  *
- * Return: pointer to the malloc'd string
+ * Return: void
  */
-char *print_float(va_list args)
+void print_float(va_list args)
 {
-	char *str = malloc(sizeof(char) * 50);
-
-	if (str == NULL)
-		return (NULL);
-
-	sprintf(str, "%f", va_arg(args, double));
-
-	return (str);
+	printf("%f", va_arg(args, double));
 }
 
 /**
- * print_string - converts a char * argument to a malloc'd string
+ * print_string - prints a char * argument, or (nil) if NULL
  * @args: the va_list to pull from
  *
- * Return: pointer to the malloc'd string, or "(nil)" copy if NULL
+ * Return: void
  */
-char *print_string(va_list args)
+void print_string(va_list args)
 {
 	char *str = va_arg(args, char *);
-	char *result;
 
 	if (str == NULL)
 		str = "(nil)";
 
-	result = malloc(sizeof(char) * (strlen(str) + 1));
-	if (result == NULL)
-		return (NULL);
-
-	strcpy(result, str);
-
-	return (result);
+	printf("%s", str);
 }
 
 /**
@@ -108,7 +77,6 @@ void print_all(const char * const format, ...)
 	};
 	va_list args;
 	unsigned int i = 0, j, printed = 0;
-	char *str;
 
 	va_start(args, format);
 
@@ -119,11 +87,9 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == ops[j].type)
 			{
-				str = ops[j].f(args);
 				if (printed)
 					printf(", ");
-				printf("%s", str);
-				free(str);
+				ops[j].f(args);
 				printed = 1;
 			}
 			j++;
