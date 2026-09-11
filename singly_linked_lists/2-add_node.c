4e1bfba1c6652fc5e70b8951a730cfd813b25274
AjT67 <14828@holbertonstudents.com>
2026-09-11 04:41:24 +0000

    hotfix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
@@ -12,6 +11,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	unsigned int len = 0;
 
 	if (head == NULL || str == NULL)
 		return (NULL);
@@ -27,7 +27,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->len = strlen(str);
+	while (str[len] != '\0')
+		len++;
+
+	new->len = len;
 	new->next = *head;
 	*head = new;
 
